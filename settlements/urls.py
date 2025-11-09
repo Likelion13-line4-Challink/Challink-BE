00bbc28f6e431dd29dda52bf1c3a9ddaf7ddff22
seokjun_kim <kimsj6978@gmail.com>
2025-11-10 04:37:40 +0900

    url slash fix

--- settlements/urls.py
+++ settlements/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import RewardStatusView, RewardClaimView, WalletChargeView
 
 urlpatterns = [
-    path("<int:challenge_id>/rewards", RewardStatusView.as_view()),
-    path("<int:challenge_id>/rewards/claim", RewardClaimView.as_view()),
+    path("<int:challenge_id>/rewards/", RewardStatusView.as_view()),
+    path("<int:challenge_id>/rewards/claim/", RewardClaimView.as_view()),
 
     # 지갑 충전
-    path("wallet/charge", WalletChargeView.as_view()),
+    path("wallet/charge/", WalletChargeView.as_view()),
 ]
